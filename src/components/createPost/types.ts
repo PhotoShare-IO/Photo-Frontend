8090395d36bde21a3f15e6026b25bb17b6558e65
vitossss <shvab7216@gmail.com>
2023-12-16 16:06:51 +0200

    add types for post creating

---
+++ src/components/createPost/types.ts
@@ -0,0 +1,16 @@
+export type Props = {
+  imageUrl: string;
+  name: string;
+  desc: string;
+};
+
+export type PostData = {
+    id: number;
+    file_url?: string;
+    name?: string;
+    description?: string;
+    album: {
+        id: number;
+        name: string;
+    }
+  };
